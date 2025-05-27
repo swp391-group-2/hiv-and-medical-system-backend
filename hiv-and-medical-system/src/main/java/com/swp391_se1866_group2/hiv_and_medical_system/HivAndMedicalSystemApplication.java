5f6a5f5af4453a66a00da199487e22ec9fc1f492
Tusry Uong <uongthanhtu5@gmail.com>
2025-05-27 23:16:20 +0700

    setupdependency

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/HivAndMedicalSystemApplication.java
@@ -0,0 +1,13 @@
+package com.swp391_se1866_group2.hiv_and_medical_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HivAndMedicalSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HivAndMedicalSystemApplication.class, args);
+	}
+
+}
