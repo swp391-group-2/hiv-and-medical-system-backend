d13d39f077adf0bff920fe7a6a7b9b4a83f42980
Tusry Uong <uongthanhtu5@gmail.com>
2025-05-29 16:11:22 +0700

    feature-login

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/common/mapper/UserMapper.java
@@ -0,0 +1,12 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.common.mapper;
+
+import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.request.UserCreationRequest;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.response.UserResponse;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.entity.User;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface UserMapper {
+    User toUser(UserCreationRequest request);
+    UserResponse toUserResponse(User user);
+}
--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/config/JwtAuthenticationEntryPoint.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/config/JwtAuthenticationEntryPoint.java
@@ -31,3 +31,4 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
         response.flushBuffer();
     }
 
+}
