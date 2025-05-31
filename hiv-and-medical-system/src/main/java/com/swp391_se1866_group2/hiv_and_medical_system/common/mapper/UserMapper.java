7b4686d845f0ae606491604f8d8d2f670780e27c
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-01 00:05:36 +0700

    fix feature-register-updateproperties: createAt & UpdateAt

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/common/mapper/UserMapper.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/common/mapper/UserMapper.java
@@ -4,9 +4,12 @@ import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.request.UserCrea
 import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.response.UserResponse;
 import com.swp391_se1866_group2.hiv_and_medical_system.user.entity.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper
 public interface UserMapper {
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
     User toUser(UserCreationRequest request);
     UserResponse toUserResponse(User user);
 }
