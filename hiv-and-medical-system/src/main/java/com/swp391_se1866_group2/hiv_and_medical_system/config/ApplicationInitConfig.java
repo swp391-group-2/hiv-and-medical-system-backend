e7c5026cde209eaf34b6ca5654600fe9f55f8863
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-01 13:50:10 +0700

    feature-create-adminaccount

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/config/ApplicationInitConfig.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/config/ApplicationInitConfig.java
@@ -1,10 +1,17 @@
 package com.swp391_se1866_group2.hiv_and_medical_system.config;
 
+import com.swp391_se1866_group2.hiv_and_medical_system.security.service.RoleService;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.entity.User;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.repository.UserRepository;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.service.UserService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -13,5 +20,22 @@ import org.springframework.context.annotation.Configuration;
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class ApplicationInitConfig {
 
+    RoleService roleService;
+    UserRepository userRepository;
 
+    @Bean
+    ApplicationRunner applicationRunner(UserService userService, RoleService roleService, PasswordEncoder passwordEncoder) {
+        return  args -> {
+            if(userRepository.findByPhoneNumber("admin").isEmpty()){
+                User user = User
+                        .builder()
+                        .phoneNumber("admin")
+                        .password(passwordEncoder.encode("admin"))
+                        .role(roleService.getOrCreateRole("ADMIN"))
+                        .build();
+                userRepository.save(user);
+                log.warn("admin user has been created with default password: admin, please change it");
+            }
+        };
+    }
 }
