8c6ca23c3423c8446a71c489d12250d086ffc051
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-03 08:44:17 +0700

    fix&config-role-permission

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/user/controller/UserController.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/user/controller/UserController.java
@@ -1,13 +1,6 @@
 package com.swp391_se1866_group2.hiv_and_medical_system.user.controller;
-
-import com.swp391_se1866_group2.hiv_and_medical_system.common.dto.ApiResponse;
-import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.request.UserCreationRequest;
-import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.response.UserResponse;
 import com.swp391_se1866_group2.hiv_and_medical_system.user.service.UserService;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,16 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/users")
 @RequiredArgsConstructor
 public class UserController {
-
     private final UserService userService;
-
-
-//    @PostMapping
-//    ApiResponse<UserResponse> createUser(@RequestBody @Valid UserCreationRequest request){
-//        return ApiResponse.<UserResponse>builder()
-//                .success(true)
-//                .result(userService.createUser(request))
-//                .build();
-//    }
 
 }
--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/user/repository/UserRepository.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/user/repository/UserRepository.java
@@ -8,6 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
-    boolean existsByPhoneNumber(String phoneNumber);
-    Optional<User> findByPhoneNumber(String phoneNumber);
+    boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
