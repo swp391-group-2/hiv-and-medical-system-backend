81f7a1722ef50975cc1b05c2df22c1f65fef40e0
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-04 17:02:29 +0700

    done feature-doctor-create by Admin and Manager

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/doctor/service/DoctorService.java
@@ -0,0 +1,51 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.doctor.service;
+
+import com.swp391_se1866_group2.hiv_and_medical_system.common.enums.UserStatus;
+import com.swp391_se1866_group2.hiv_and_medical_system.common.exception.ErrorCode;
+import com.swp391_se1866_group2.hiv_and_medical_system.common.mapper.DoctorMapper;
+import com.swp391_se1866_group2.hiv_and_medical_system.doctor.dto.request.DoctorCreationRequest;
+import com.swp391_se1866_group2.hiv_and_medical_system.doctor.dto.response.DoctorResponse;
+import com.swp391_se1866_group2.hiv_and_medical_system.doctor.entity.Doctor;
+import com.swp391_se1866_group2.hiv_and_medical_system.doctor.repository.DoctorRepository;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.dto.request.UserCreationRequest;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.entity.User;
+import com.swp391_se1866_group2.hiv_and_medical_system.user.service.UserService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class DoctorService {
+    DoctorRepository doctorRepository;
+    UserService userService;
+    DoctorMapper doctorMapper;
+
+    PasswordEncoder passwordEncoder;
+
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER')")
+    public DoctorResponse createDoctorAccount (DoctorCreationRequest request, String role){
+        if(userService.isEmailExisted(request.getEmail())){
+            throw new RuntimeException(ErrorCode.EMAIL_EXISTED.getMessage());
+        }
+        System.out.println(request);
+        Doctor doctor = doctorMapper.toDoctor(request) ;
+        User user = new User();
+        user.setRole(role);
+        user.setEmail(request.getEmail());
+        user.setFullName(request.getFullName());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setStatus(UserStatus.ACTIVE.name());
+        doctor.setUser(user);
+        System.out.println(doctor);
+        return doctorMapper.toDoctorResponse(doctorRepository.save(doctor));
+    }
+
+
+}
