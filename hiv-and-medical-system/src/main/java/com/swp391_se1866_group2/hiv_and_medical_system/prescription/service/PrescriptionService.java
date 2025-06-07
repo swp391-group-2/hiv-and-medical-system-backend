c08670d371b69a2384691063537ef98d604fc665
phamquan1301 <pquan3278@gmail.com>
2025-06-07 23:59:42 +0700

    done crud-prescription

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/prescription/service/PrescriptionService.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/prescription/service/PrescriptionService.java
@@ -4,6 +4,7 @@ import com.swp391_se1866_group2.hiv_and_medical_system.common.exception.AppExcep
 import com.swp391_se1866_group2.hiv_and_medical_system.common.exception.ErrorCode;
 import com.swp391_se1866_group2.hiv_and_medical_system.common.mapper.PrescriptionMapper;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.dto.request.PrescriptionRequest;
+import com.swp391_se1866_group2.hiv_and_medical_system.prescription.dto.request.PrescriptionUpdateRequest;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.dto.response.PrescriptionResponse;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.entity.Prescription;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.repository.PrescriptionRepository;
@@ -12,6 +13,9 @@ import lombok.experimental.FieldDefaults;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -29,6 +33,36 @@ public class PrescriptionService {
 
         Prescription prescription = prescriptionMapper.toPrescription(request);
         return prescriptionMapper.toPrescriptionResponse(prescriptionRepository.save(prescription));
+    }
+
+    @PreAuthorize("hasRole('DOCTOR') or hasRole('PATIENT') or hasRole('STAFF') or hasRole('ADMIN') or hasRole('MANAGER')")
+    public PrescriptionResponse getPrescription(String prescriptionId) {
+        Prescription prescription = prescriptionRepository.findById(prescriptionId)
+                .orElseThrow(() -> new AppException(ErrorCode.PRESCRIPTION_NOT_EXISTED));
+        return prescriptionMapper.toPrescriptionResponse(prescription);
+    }
+
+    @PreAuthorize("hasRole('DOCTOR') or hasRole('PATIENT') or hasRole('STAFF') or hasRole('ADMIN') or hasRole('MANAGER')")
+    public List <PrescriptionResponse> getAllPrescriptions() {
+        return prescriptionRepository.findAll().stream()
+                .map(prescriptionMapper::toPrescriptionResponse)
+                .collect(Collectors.toList());
+    }
+
+    @PreAuthorize("hasRole('DOCTOR')")
+    public PrescriptionResponse updatePrescription(String prescriptionId, PrescriptionUpdateRequest request) {
+        Prescription prescription = prescriptionRepository.findById(prescriptionId)
+                .orElseThrow(() -> new AppException(ErrorCode.PRESCRIPTION_NOT_EXISTED));
+
+        prescriptionMapper.updatePrescription(prescription, request);
+        return prescriptionMapper.toPrescriptionResponse(prescriptionRepository.save(prescription));
+    }
+
+    @PreAuthorize("hasRole('DOCTOR')")
+    public void deletePrescription(String prescriptionId) {
+        Prescription prescription = prescriptionRepository.findById(prescriptionId)
+                .orElseThrow(() -> new AppException(ErrorCode.PRESCRIPTION_NOT_EXISTED));
+        prescriptionRepository.deleteById(prescriptionId);
     }
 
 
