5a304bb8c6de9ae8cec0c3b558fbe69d2c8c56b2
phamquan1301 <pquan3278@gmail.com>
2025-06-07 23:59:42 +0700

    done crud-prescription

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/prescription/controller/PrescriptionController.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/prescription/controller/PrescriptionController.java
@@ -2,16 +2,16 @@ package com.swp391_se1866_group2.hiv_and_medical_system.prescription.controller;
 
 import com.swp391_se1866_group2.hiv_and_medical_system.common.dto.ApiResponse;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.dto.request.PrescriptionRequest;
+import com.swp391_se1866_group2.hiv_and_medical_system.prescription.dto.request.PrescriptionUpdateRequest;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.dto.response.PrescriptionResponse;
 import com.swp391_se1866_group2.hiv_and_medical_system.prescription.service.PrescriptionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.security.access.method.P;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/prescriptions")
@@ -26,6 +26,39 @@ public class PrescriptionController {
         return ApiResponse.<PrescriptionResponse>builder()
                 .success(true)
                 .result(prescriptionService.createPrescription(request))
+                .build();
+    }
+
+    @GetMapping("{prescriptionId}")
+    public ApiResponse<PrescriptionResponse> getPrescription(@PathVariable String prescriptionId) {
+        return ApiResponse.<PrescriptionResponse>builder()
+                .success(true)
+                .result(prescriptionService.getPrescription(prescriptionId))
+                .build();
+    }
+
+    @GetMapping
+    public ApiResponse<List<PrescriptionResponse>> getAllPrescriptions() {
+        return ApiResponse.<List<PrescriptionResponse>>builder()
+                .success(true)
+                .result(prescriptionService.getAllPrescriptions())
+                .build();
+    }
+
+    @PutMapping("{prescriptionId}")
+    public ApiResponse<PrescriptionResponse> updatePrescription(@PathVariable String prescriptionId, @RequestBody @Valid PrescriptionUpdateRequest request) {
+        return ApiResponse.<PrescriptionResponse>builder()
+                .success(true)
+                .result(prescriptionService.updatePrescription(prescriptionId, request))
+                .build();
+    }
+
+    @DeleteMapping("{prescriptionId}")
+    public ApiResponse<Void> deletePrescription(@PathVariable String prescriptionId) {
+        prescriptionService.deletePrescription(prescriptionId);
+        return ApiResponse.<Void>builder()
+                .success(true)
+                .result(null)
                 .build();
     }
 }
