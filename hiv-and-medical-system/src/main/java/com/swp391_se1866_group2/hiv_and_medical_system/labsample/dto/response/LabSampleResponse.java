e125bf55a97fddfed3984d7febe3aeda3f480fa5
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-11 00:32:43 +0700

    done lab_sample get, getall, create, update

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/labsample/dto/response/LabSampleResponse.java
@@ -0,0 +1,19 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.labsample.dto.response;
+
+import com.swp391_se1866_group2.hiv_and_medical_system.common.enums.LabSampleStatus;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LabSampleResponse {
+    int id;
+    String sampleCode;
+    String sampleType;
+    LocalDateTime collectedAt;
+    String status;
+}
