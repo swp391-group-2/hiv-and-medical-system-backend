26cc0891111357d271b3844f08337904f05890ef
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-11 00:32:43 +0700

    done lab_sample get, getall, create, update

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/labsample/dto/request/LabSampleCreationRequest.java
@@ -0,0 +1,14 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.labsample.dto.request;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class LabSampleCreationRequest {
+    String sampleCode;
+    String sampleType;
+}
