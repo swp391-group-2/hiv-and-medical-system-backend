a611d045571d061dae6520eed7f23d499278274d
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-10 13:42:41 +0700

    done get schedule fromdate-todate by doctor token

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/schedule/dto/response/ScheduleResponse.java
@@ -0,0 +1,17 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.schedule.dto.response;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDate;
+import java.util.Set;
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class ScheduleResponse {
+    int id;
+    LocalDate workDate;
+    Set<ScheduleSlotResponse> scheduleSlots;
+}
