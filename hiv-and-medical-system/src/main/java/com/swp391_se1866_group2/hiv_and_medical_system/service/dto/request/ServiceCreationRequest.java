016916c87071b35e11ef3235dca71fff99b272e3
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-11 09:36:59 +0700

    done createService

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/service/dto/request/ServiceCreationRequest.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/service/dto/request/ServiceCreationRequest.java
@@ -1,4 +1,16 @@
 package com.swp391_se1866_group2.hiv_and_medical_system.service.dto.request;
 
+import com.swp391_se1866_group2.hiv_and_medical_system.common.enums.ServiceType;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class ServiceCreationRequest {
+    String name;
+    double price;
+    ServiceType serviceType;
 }
