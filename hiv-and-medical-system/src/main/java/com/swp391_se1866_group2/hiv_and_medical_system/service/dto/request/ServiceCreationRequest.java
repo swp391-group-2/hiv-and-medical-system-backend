70d7dbcb112709af06760d32ecceab85c7cdb88a
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-11 08:37:56 +0700

    basic payment & service

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/service/dto/request/ServiceCreationRequest.java
@@ -0,0 +1,4 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.service.dto.request;
+
+public class ServiceCreationRequest {
+}
---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/service/dto/response/ServiceResponse.java
@@ -0,0 +1,4 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.service.dto.response;
+
+public class ServiceResponse {
+}
