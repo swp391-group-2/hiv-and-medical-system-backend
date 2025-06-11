446625c32102316bcace39963b2d0aa747bfec07
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-11 13:36:14 +0700

    fix feature-labsample

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/appointment/dto/response/AppointmentResponse.java
@@ -0,0 +1,4 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.appointment.dto.response;
+
+public class AppointmentResponse {
+}
--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/appointment/entity/Appointment.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/appointment/entity/Appointment.java
@@ -1,8 +1,50 @@
 package com.swp391_se1866_group2.hiv_and_medical_system.appointment.entity;
 
+import com.swp391_se1866_group2.hiv_and_medical_system.common.enums.AppoimentStatus;
+import com.swp391_se1866_group2.hiv_and_medical_system.doctor.entity.Doctor;
+import com.swp391_se1866_group2.hiv_and_medical_system.labsample.entity.LabSample;
 import com.swp391_se1866_group2.hiv_and_medical_system.patient.entity.Patient;
+import com.swp391_se1866_group2.hiv_and_medical_system.prescription.entity.Prescription;
+import com.swp391_se1866_group2.hiv_and_medical_system.schedule.consultation.entity.ScheduleSlot;
+import com.swp391_se1866_group2.hiv_and_medical_system.schedule.laboratory.entity.LabTestSlot;
+import com.swp391_se1866_group2.hiv_and_medical_system.service.entity.ServiceEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     int id;
+    @ManyToOne(fetch = FetchType.LAZY)
     Patient patient;
+    @ManyToOne(fetch = FetchType.LAZY)
+    LabTestSlot labTestSlot;
+    @ManyToOne(fetch = FetchType.LAZY)
+    Doctor doctor;
+    @ManyToOne(fetch = FetchType.LAZY)
+    ServiceEntity service;
+    @OneToOne(fetch = FetchType.LAZY)
+    Prescription prescription;
+    @ManyToOne(fetch = FetchType.LAZY)
+    ScheduleSlot scheduleSlot;
+    @OneToOne(fetch = FetchType.LAZY)
+    LabSample labSample;
+    @Enumerated(EnumType.STRING)
+    AppoimentStatus status;
+    @CreationTimestamp
+    @Column(updatable = false)
+    LocalDateTime createdAt;
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
 }
