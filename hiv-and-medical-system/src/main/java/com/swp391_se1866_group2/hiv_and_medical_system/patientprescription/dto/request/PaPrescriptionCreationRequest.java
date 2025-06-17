dde07682e28ab312ed1bdbcc4f24a23fe83e6070
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-17 23:30:09 +0700

    setup patientPrescription and item

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/patientprescription/dto/request/PaPrescriptionCreationRequest.java
@@ -0,0 +1,14 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.patientprescription.dto.request;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PaPrescriptionCreationRequest {
+}
---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/patientprescription/dto/response/PaPrescriptionResponse.java
@@ -0,0 +1,17 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.patientprescription.dto.response;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class PaPrescriptionResponse {
+    int id;
+    int duration;
+    String note;
+}
