5bc6794387e0c001aed41546dd26e517e1756dd5
Tusry Uong <uongthanhtu5@gmail.com>
2025-06-17 23:30:09 +0700

    setup patientPrescription and item

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/patientprescription/entity/PatientPrescription.java
@@ -0,0 +1,36 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.patientprescription.entity;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import com.swp391_se1866_group2.hiv_and_medical_system.appointment.entity.Appointment;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class PatientPrescription {
+    @Id
+    int id;
+    int duration;
+    @OneToOne
+    Appointment appointment;
+    @CreationTimestamp
+    @Column(updatable = false)
+    String note;
+    @OneToMany
+    @JsonManagedReference("patientPrescriptionItems-patientPrescription")
+    List<PatientPrescriptionItem> patientPrescriptionItems;
+    LocalDateTime createdAt;
+    @UpdateTimestamp
+    LocalDateTime updatedAt;
+
+}
---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/patientprescription/service/PatientPrescriptionService.java
@@ -0,0 +1,7 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.patientprescription.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PatientPrescriptionService {
+}
