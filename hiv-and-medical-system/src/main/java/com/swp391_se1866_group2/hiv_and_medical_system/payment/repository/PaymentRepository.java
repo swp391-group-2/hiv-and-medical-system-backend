203b80dd533d9b0aba03642f2bdc540093f8a6a3
phamquan1301 <pquan3278@gmail.com>
2025-06-25 16:06:37 +0700

    done

--- hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/payment/repository/PaymentRepository.java
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/payment/repository/PaymentRepository.java
@@ -2,12 +2,21 @@ package com.swp391_se1866_group2.hiv_and_medical_system.payment.repository;
 
 import com.swp391_se1866_group2.hiv_and_medical_system.payment.entity.Payment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface PaymentRepository extends JpaRepository<Payment, Integer> {
     Optional<Payment> findById(int id);
 
     Optional<Payment> findBySessionId(String sessionId);
+
+    @Query("SELECT COALESCE(SUM(p.amount), 0) FROM Payment p WHERE p.status = 'PAID'")
+    long getTotalRevenue();
+
+    @Query("SELECT COALESCE(SUM(p.amount), 0) FROM Payment p WHERE p.status = 'PAID' AND DATE(p.succeededAt) <= :milestone")
+    long getRevenue(@Param("milestone") LocalDate milestone);
 
 }
