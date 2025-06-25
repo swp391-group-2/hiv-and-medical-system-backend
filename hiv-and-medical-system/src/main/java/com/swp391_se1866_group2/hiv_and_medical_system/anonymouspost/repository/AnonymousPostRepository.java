5e72611eaa67c6378693ae13e2dca7291f8d8640
phamquan1301 <pquan3278@gmail.com>
2025-06-25 11:35:35 +0700

    done

---
+++ hiv-and-medical-system/src/main/java/com/swp391_se1866_group2/hiv_and_medical_system/anonymouspost/repository/AnonymousPostRepository.java
@@ -0,0 +1,9 @@
+package com.swp391_se1866_group2.hiv_and_medical_system.anonymouspost.repository;
+
+import com.swp391_se1866_group2.hiv_and_medical_system.anonymouspost.entity.AnonymousPost;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnonymousPostRepository extends JpaRepository<AnonymousPost, Integer> {
+}
